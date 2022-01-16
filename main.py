afb3c2ce71a99a089746d8012ba2b61a0f9ba9bf
gaciuki2402 <delbrivigrace@gmail.com>
2022-01-16 17:34:54 +0300

    improt time

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
+from datetime import datetime
+class Birthday(object):
+    def Friends(self,number_of_birthdays):
+        current_date=datetime.now().date()
+        my_friends={
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Grace')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        }
+        for birth_day in range(1,number_of_birthdays+1):
+            name=input("Name:")
+            date=str(input("Enter date of birth (yyyy-mm-dd):"))
+            date_of_birth=datetime.strptime(date,"%Y-%m-%d").date()
+            location=input("Location:")
+            age=current_date.year-date_of_birth.year
+            hotel=input("Enter the Hotel:")
+            my_friends["Name"]=name
+            my_friends["DOB"]=f"{date_of_birth.year}-{date_of_birth.month}-{date_of_birth.day}"
+            my_friends["Age"]=age
+            my_friends["Location"]=location
+            my_friends["Hotel"]=hotel
+        print(my_friends)
+b1=Birthday()
+b1.Friends(1)
