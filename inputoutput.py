cba6e8ddf6a27e74a8688d5f149455604fa6c1ad
gaciuki2402 <delbrivigrace@gmail.com>
2022-01-20 10:31:41 +0300

    initial commit

---
+++ inputoutput.py
@@ -0,0 +1,31 @@
+# name=input("What is your name?")
+# print(name)
+# print("Welcome Grace.")
+# n=int(input("Enter a number:"))
+# print(n+100)
+fname="gaciuki"
+lname="davie"
+print("name:",fname,lname)
+fname="lexi"
+lname="roxy."
+print(fname,lname)
+days={1:"monday",2:"tuesday",3:"wednesday",4:"thursday",5:"friday"}
+for k,v in days.items():
+    print(k,v, sep="-->")
+if True:
+    print("thursday",end='/')
+    print(4,end='/')
+    print("friday")
+for n in range(10):
+    print(n)
+print(11%3)
+print("-"*67)
+print(ord("a"))
+print(ord("#"))
+print(ord("t"))
+print(chr(23))
+print(chr(90))
+print(chr(35))
+
+
+
