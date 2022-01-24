a34798520ab2a0e7aa289951a1b42485f9a9e1ae
gaciuki2402 <delbrivigrace@gmail.com>
2022-01-24 17:42:00 +0300

    strings

--- generators.py
+++ generators.py
@@ -10,14 +10,34 @@ def integers_starting_from(n):
         yield n
         n +=1
 
-x=[1,2,3,4,5,6,7,8,9,10]
-print(sys.getsizeof(x))
+w=[1,2,3,4,5,6,7,8,9,10]
+print(sys.getsizeof(w))
 print(sys.getsizeof(range(1,11)))
-for element in x:
+for element in w:
     print(element)
 print("-"*67)
 
 for i in range(1,11):
     print(i)
 
-
+from itertools import islice
+multiples_of_four=(x*4 for x in integers_starting_from(1))
+first_ten_multiples_of_four=list(islice(multiples_of_four,10))
+print(first_ten_multiples_of_four)
+print("*"*56)
+multiples_of_three=(w*3 for w in integers_starting_from(1))
+first_10_multiples_of_three=list(islice(multiples_of_three,10))
+print(first_10_multiples_of_three)
+for idx, number in enumerate(multiples_of_three):
+    print(number)
+    if idx==9:
+        break
+#fibonacci
+import itertools
+def n():
+    a, b=1, 1
+    while True:
+        yield a
+        a, b=b, a+b
+first_ten_fibs=list(itertools.islice(n(), 10))
+print(first_ten_fibs)
---
+++ strings.py
@@ -0,0 +1,65 @@
+print(ord("A"))
+print(ord("a"))
+print(ord("#"))
+print(chr(87))
+print(chr(35))
+print(chr(8364))
+print(chr(8721))
+x="i am 10 years old"
+print(len(x))
+print(str(373.098))
+print(str("fgjskj"))
+s="grace"
+print(s[1])
+print(len(s))
+print(s[len(s)-2])
+print(s[-2])
+print(s[0:3])
+s=s[:4]+s[4:]==s
+print(s)
+w="@david"
+print(w[0:6:2])
+g="373920"*3
+print(g)
+p="am a student at TTU."
+print(p[::-1])
+n=34
+m=20
+prod=n*m
+print(n,"*",m,"=",prod)
+var="Bark"
+print(f"a dog says {var}!")
+w=w[:3]+"s" +w[4:]
+print(w)
+t="graceGACIUki"
+print("+"*67)
+t1="grace1234".isalnum()
+print(t1)
+t2="grace@gaciuki$".isalpha()
+t3="12345".isdigit()
+print(t3)
+print(t2)
+print(t.capitalize())
+print(t.swapcase())
+m="the sun is fading away and that's the end of the day."
+print(m.title())
+n="as the june light turns to moon light i'll be on my way.".title()
+print(n)
+print(n.count("Light"))
+n2="Light".endswith("ght")
+print(n2)
+n3="light".endswith("gh")
+print(n3)
+l="grace irene belta day gaciuki belta peter belta angie purity"
+print(l.find("belta"))
+fruits=["oranges","apples","lemon","pineapple","avocado"]
+print(','.join(fruits))
+print(list(fruits))
+p="www.realpython.com".rsplit(sep='.')
+print(p)
+
+
+
+
+
+
