5ba71be6113735b623f99f08b34ce4687af28fae
gaciuki2402 <delbrivigrace@gmail.com>
2022-01-24 11:19:11 +0300

    datetime4

--- time4.py
+++ time4.py
@@ -9,20 +9,30 @@ class Bell_intervals():
             unit=input("Enter Unit name:")
             lecturer=input("LecName:")
             room=input("RoomNo:")
-            start_of_lesson=input("Start: (HH:MM:SS")
-            start=datetime.strptime(start_of_lesson, "%H:%M:%S").time()
-            time_take=input("Duration: (HH:MM:SS)")
-            time_taken=f"{time_take}:00:00"
-            duration = datetime.strptime(time_taken, "%H:%M:%S").time()
+            start_of_lesson=input("Start: (HH:MM:SS)")
+            start=datetime.strptime(start_of_lesson, "%H:%M:%S")
+            print(type(start))
+            time_taken=input("Duration: (HH:MM)")
+            if time_taken.__contains__(":"):
+                t=time_taken.split(":")
+                #t=['2','30']
+                hour=int(t[0])
+                minute=int(t[1])
+            else:
+                minute = 0
+                hour = int(time_taken)
+            duration=timedelta(hours=hour,minutes=minute)
             print(type(duration))
-            End_of_sesion=start+duration
+            End_of_session= start+duration
+
+
             details["Unit"]=unit
             details["LecName"]=lecturer
             details["RoomNO"]=room
-            details["Start"]=str(start)
-            details["Duration"]=str(time_taken)
-            details["End"]=End_of_sesion
-        j=json.dumps(details)
+            details["Start"]=str(start.time())
+            details["Duration"]=str(duration)
+            details["End"]=str(End_of_session.time())
+        j=json.dumps(details,indent=4)
         print(j)
 
 c1=Bell_intervals()
